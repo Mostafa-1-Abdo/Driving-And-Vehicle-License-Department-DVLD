f4d46266d6b8f184c577976545de363124d33949
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-08-17 22:29:15 +0300

    Build the project structure and complete the main form

---
+++ DVLD.Logic/clPerson.cs
@@ -0,0 +1,130 @@
+﻿using DVLD.Data.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD.Data
+{
+    public class clPerson
+    {
+        public enum enMode : byte
+        {
+            AddNew, Update
+        }
+        public enum enGender : byte
+        {
+            Male, Female
+        }
+
+        public enMode _Mode;
+
+        public int ID { get; set; }
+        public enGender Gender { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int CountryID { get; set; }
+        public string NationalNumber { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ImagePath { get; set; }
+
+        public clPersonDTO PersonDTO
+        {
+            get
+            {
+                return new clPersonDTO(ID, (byte)Gender, FirstName, SecondName, ThirdName, LastName, DateOfBirth, CountryID, NationalNumber, Address, Phone, Email, ImagePath);
+            }
+        }
+
+        private clPerson(clPersonDTO PersonDTO)
+        {
+            _Mode = enMode.Update;
+
+            ID = PersonDTO.ID;
+            Gender = (enGender)PersonDTO.Gender;
+            FirstName = PersonDTO.FirstName;
+            SecondName = PersonDTO.SecondName;
+            ThirdName = PersonDTO.ThirdName;
+            LastName = PersonDTO.LastName;
+            DateOfBirth = PersonDTO.DateOfBirth;
+            CountryID = PersonDTO.CountryID;
+            NationalNumber = PersonDTO.NationalNumber;
+            Address = PersonDTO.Address;
+            Phone = PersonDTO.Phone;
+            Email = PersonDTO.Email;
+            ImagePath = PersonDTO.ImagePath;
+        }
+        public clPerson()
+        {
+            _Mode = enMode.AddNew;
+
+            ID = -1;
+            Gender = enGender.Male;
+            FirstName = string.Empty;
+            SecondName = string.Empty;
+            ThirdName = string.Empty;
+            LastName = string.Empty;
+            DateOfBirth = DateTime.MinValue;
+            CountryID = -1;
+            NationalNumber = string.Empty;
+            Address = string.Empty;
+            Phone = string.Empty;
+            Email = string.Empty;
+            ImagePath = string.Empty;
+        }
+
+        static public clPerson Find(int ID)
+        {
+            clPersonDTO PersonDTO = clPersonData.Find(ID);
+
+            return PersonDTO != null ? new clPerson(PersonDTO) : null;
+        }
+
+        private bool _AddNew()
+        {
+            ID = clPersonData.AddNew(PersonDTO);
+
+            return ID != -1;
+        }
+        private bool _Update()
+        {
+            return clPersonData.Update(PersonDTO);
+        }
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew())
+                    {
+                        _Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+
+                case enMode.Update:
+                    return _Update();
+
+                default:
+                    return false;
+            }
+        }
+
+        public static bool Delete(int ID)
+        {
+            return clPersonData.Delete(ID);
+        }
+
+        public static DataTable GetAllPeople()
+        {
+            return clPersonData.GetAllPeople();
+        }
+    }
+}
---
+++ DVLD.UI/ctrlManageData.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DVLD.UI
+{
+    partial class ctrlManageData
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Title = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_NumberOfRecords = new System.Windows.Forms.Label();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pb_title = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_title)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.BackgroundColor = System.Drawing.Color.White;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 211);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(941, 262);
+            this.dgv.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filter By:";
+            // 
+            // lb_Title
+            // 
+            this.lb_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Title.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lb_Title.Location = new System.Drawing.Point(384, 157);
+            this.lb_Title.Name = "lb_Title";
+            this.lb_Title.Size = new System.Drawing.Size(197, 25);
+            this.lb_Title.TabIndex = 2;
+            this.lb_Title.Text = "label2";
+            this.lb_Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(56, 187);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(153, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 187);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(153, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 482);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Records:";
+            // 
+            // lb_NumberOfRecords
+            // 
+            this.lb_NumberOfRecords.AutoSize = true;
+            this.lb_NumberOfRecords.Location = new System.Drawing.Point(65, 482);
+            this.lb_NumberOfRecords.Name = "lb_NumberOfRecords";
+            this.lb_NumberOfRecords.Size = new System.Drawing.Size(0, 13);
+            this.lb_NumberOfRecords.TabIndex = 8;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Add.Location = new System.Drawing.Point(890, 157);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(63, 47);
+            this.btn_Add.TabIndex = 9;
+            this.btn_Add.UseVisualStyleBackColor = true;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Image = global::DVLD.UI.Properties.Resources.Close;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Close.Location = new System.Drawing.Point(867, 479);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(86, 25);
+            this.btn_Close.TabIndex = 6;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            // 
+            // pb_title
+            // 
+            this.pb_title.Location = new System.Drawing.Point(384, 13);
+            this.pb_title.Name = "pb_title";
+            this.pb_title.Size = new System.Drawing.Size(197, 141);
+            this.pb_title.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_title.TabIndex = 5;
+            this.pb_title.TabStop = false;
+            // 
+            // ctrlManageData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.lb_NumberOfRecords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.pb_title);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lb_Title);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Name = "ctrlManageData";
+            this.Size = new System.Drawing.Size(965, 513);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_title)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_Title;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pb_title;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_NumberOfRecords;
+        private System.Windows.Forms.Button btn_Add;
+    }
+}
