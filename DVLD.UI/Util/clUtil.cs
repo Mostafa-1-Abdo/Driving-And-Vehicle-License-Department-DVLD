d5033f968703facd9a04fc281e33030d7c21e569
Mostafa Abdo <mostafaaabdo4789@gmail.com>
2026-08-20 22:32:49 +0300

    Add email validation in frmAdd/EditPerson

---
+++ DVLD.UI/Util/clUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DVLD.UI.Util
+{
+    public static class clUtil
+    {
+       public static bool IsValidEmail(string Email)
+        {
+            return Regex.IsMatch(Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+        }
+    }
+}
