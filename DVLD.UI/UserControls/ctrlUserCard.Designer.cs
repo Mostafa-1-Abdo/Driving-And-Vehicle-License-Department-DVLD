67412ef8b576c3a7d1cf35fa9fa27cbbc754df8e
Mostafa Abdo <mostafaaabdo4789@gmail.com>
2026-08-22 07:23:41 +0300

    Complete show user deatils form

---
+++ DVLD.UI/UserControls/ctrlUserCard.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DVLD.UI.UserControls
+{
+    partial class ctrlUserCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlPersonCard1 = new DVLD.UI.UserControls.ctrlPersonCard();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb_IsActive = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_Username = new System.Windows.Forms.Label();
+            this.lb_ID = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(0, 0);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(818, 273);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_IsActive);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lb_Username);
+            this.groupBox1.Controls.Add(this.lb_ID);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 279);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(818, 86);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login Information";
+            // 
+            // lb_IsActive
+            // 
+            this.lb_IsActive.AutoSize = true;
+            this.lb_IsActive.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_IsActive.Location = new System.Drawing.Point(668, 37);
+            this.lb_IsActive.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_IsActive.Name = "lb_IsActive";
+            this.lb_IsActive.Size = new System.Drawing.Size(53, 20);
+            this.lb_IsActive.TabIndex = 140;
+            this.lb_IsActive.Text = "[????]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(567, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 139;
+            this.label2.Text = "Is Active : ";
+            // 
+            // lb_Username
+            // 
+            this.lb_Username.AutoSize = true;
+            this.lb_Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Username.Location = new System.Drawing.Point(408, 37);
+            this.lb_Username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_Username.Name = "lb_Username";
+            this.lb_Username.Size = new System.Drawing.Size(53, 20);
+            this.lb_Username.TabIndex = 138;
+            this.lb_Username.Text = "[????]";
+            // 
+            // lb_ID
+            // 
+            this.lb_ID.AutoSize = true;
+            this.lb_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ID.Location = new System.Drawing.Point(145, 37);
+            this.lb_ID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lb_ID.Name = "lb_ID";
+            this.lb_ID.Size = new System.Drawing.Size(53, 20);
+            this.lb_ID.TabIndex = 137;
+            this.lb_ID.Text = "[????]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(94, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 136;
+            this.label4.Text = "ID : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(304, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 130;
+            this.label1.Text = "Username:";
+            // 
+            // ctrlUserCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "ctrlUserCard";
+            this.Size = new System.Drawing.Size(821, 442);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lb_IsActive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_Username;
+        private System.Windows.Forms.Label lb_ID;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+    }
+}
