2c139b23cad868f11698b99d5bb6100090fd14ab
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-08-27 23:01:15 +0300

    Create clTestTypeDTO

---
+++ DVLD.Data/DTOs/clTestTypesDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace DVLD.Data.DTOs
+{
+    public class clTestTypeDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Fees { get; set; }
+
+        public clTestTypeDTO()
+        {
+            ID = -1;
+            Name = string.Empty;
+            Description = string.Empty;
+            Fees = 0;
+        }
+        public clTestTypeDTO(int id, string name,string description,decimal fees)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+            Fees = fees;
+        }
+    }
+}
