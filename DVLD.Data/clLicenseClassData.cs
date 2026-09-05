b2d06af9cd1c585d3d9673e38855c3dfb52894bc
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-09-05 17:33:50 +0300

    Create clLicenseClassData

---
+++ DVLD.Data/clLicenseClassData.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using static DVLD.Data.clDataAccessSettings;
+
+namespace DVLD.Data
+{
+    public static class clLicenseClassData
+    {
+        public static DataTable GetAllLicenseClasses()
+        {
+            DataTable licenseClassesTable = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string sql = "select * from LicenseClasses";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            licenseClassesTable.Load(reader);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return licenseClassesTable;
+        }
+    }
+}
