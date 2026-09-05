4d5f8386a1c041a3a2d8e39e67872718c0a1d371
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-09-05 14:08:17 +0300

    Create clApplicationTypeDTO

---
+++ DVLD.Data/DTOs/clApplicationTypeDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DVLD.Data.DTOs
+{
+    public class clApplicationTypeDTO
+    {
+        public int ID { get; set; } = -1;
+        public string Title { get; set; } = string.Empty;
+        public decimal Fees { get; set; } = 0;
+
+        public clApplicationTypeDTO() { }
+        public clApplicationTypeDTO(int id, string title, decimal fees)
+        {
+            ID = id;
+            Title = title;
+            Fees = fees;
+        }
+    }
+}
