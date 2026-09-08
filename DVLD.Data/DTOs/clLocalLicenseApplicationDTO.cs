3b439a5a42bf49d661b712e8d44af4a0293c158b
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-09-08 19:13:17 +0300

    Create clLocalLicenseApplicationsDTO

---
+++ DVLD.Data/DTOs/clLocalLicenseApplicationDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DVLD.Data.DTOs
+{
+    public class clLocalLicenseApplicationDTO
+    {
+        public int ID { get; set; } = -1;
+        public int ApplicationID { get; set; } = -1;
+        public int LicenseClassID { get; set; } = -1;
+
+        public clLocalLicenseApplicationDTO() { }
+        public clLocalLicenseApplicationDTO(int id, int applicationID, int licenseClassID)
+        {
+            ID = id;
+            ApplicationID = applicationID;
+            LicenseClassID = licenseClassID;
+        }
+    }
+}
