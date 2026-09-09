4c752cadb3feb66ee6270e1e072ae96cc4d1109c
Mostafa Abdo <mostafaabdo4789@gmail.com>
2026-09-09 23:45:50 +0300

    feat(data): add clTestDTO

---
+++ DVLD.Data/DTOs/clTestDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace DVLD.Data.DTOs
+{
+    public class clTestDTO
+    {
+        public int ID { get; set; } = -1;
+        public int AppointmentID { get; set; } = -1;
+        public bool Result { get; set; } = false;
+        public string Notes { get; set; } = null;
+        public int UserID { get; set; } = -1;
+
+        public clTestDTO() { }
+        public clTestDTO(int id,int appointmentID,bool result,string notes,int userID)
+        {
+            ID = id;
+            AppointmentID = appointmentID;
+            Result = result;
+            Notes = notes;
+            UserID = userID;
+        }
+    }
+}
